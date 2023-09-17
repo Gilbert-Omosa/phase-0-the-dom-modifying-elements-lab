d9bd39be199d9c9fda0afcbb68c8209afcc369e0
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-17 15:52:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove('main#main');
+
+const newHeader = document.createElement(`h1`);
+
+newHeader.id = `victory`;
+
+newHeader.textContent = `Gilbert is the champion`;
